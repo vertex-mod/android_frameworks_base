81c308fdc0f877072331f68a9b2a86e531ca3bf5
BigBrother1984 <carlosavignano@aospa.co>
2016-03-26 16:55:22 -0400

    Improve Heads up toggle [1/2]
    * Add multiuser support, provide a system setting to override heads up
      showing. User can disable heads up within this one so we don't need to
      mess with Settings.Global heads up dependencies and break Google wide
      implementations like priority filtering etc.
    *updated heads up quicksetting tile to reflect changes as well
    
    PureNexus Edits:
    *Adapted for Marshmallow/Nougat

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3609,6 +3609,23 @@ public final class Settings {
          */
 
         /**
+         * Whether the user has enabled headsup (Default 1)
+         * HeadsUp is enabled by default within its Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED
+         * setting. Avoid changing low level implementations and use a System setting to
+         * override per-user heads up on/off preference.
+         *
+         * Enable headsup = 1 (on by default)
+         * Disable headsup = 0
+         *
+         * @see com.android.systemui.statusbar.BaseStatusBar#addNotification
+         * @hide
+         */
+        public static final String HEADS_UP_USER_ENABLED = "heads_up_user_enabled";
+
+        /** @hide */ public static final int HEADS_UP_USER_OFF = 0;
+        /** @hide */ public static final int HEADS_UP_USER_ON = 1;
+
+        /**
          * Whether or not volume button music controls should be enabled to seek media tracks
          * @hide
          */
