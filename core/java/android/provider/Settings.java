2485f96db1b6b0bf52fc16d62364c2304a90696f
maxwen <max.weninger@gmail.com>
2016-10-31 23:48:50 +0100

    base: SystemUI: make QS columns count configurable
    Adapted for VertexOS.
    
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    PS4
    we must explicitly hide the columns submenu
    
    Change-Id: Id033a5a2bcd983d7704923e760bb8941c91be013

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3694,6 +3694,11 @@ public final class Settings {
         public static final String POWERMENU_POWER = "powermenu_power";
 
         /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
