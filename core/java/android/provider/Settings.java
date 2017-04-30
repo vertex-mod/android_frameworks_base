c193cc4a15d18d8189739351608a89d7c8ffe265
joshuous <joshuous@gmail.com>
2017-04-30 16:17:12 +0000

    [1/2] unlink notification volume is back
    -delete old unused volume images
    -add new one for notification volumes
    maybe some day one will be able to create a vector
    drawable for this - I am not :)
    
    Change-Id: Id26021832ad1ca5e579878ee2471f7c401bc74ac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3630,6 +3630,13 @@ public final class Settings {
         };
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * These are all public system settings
          *
          * @hide
