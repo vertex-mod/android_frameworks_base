8263b650f56a29dc802bbb71832a1156b0ce4290
joshuous <joshuous@gmail.com>
2017-05-09 03:13:31 +0000

    [SQUASHED] Remove BoostFramework
    We boost via PowerHAL instead.
    
    Revert "BoostFramework: Logs for IO prefetcher"
    
    This reverts commit f1d511e5a4c45c38c757fdff269f89885bd4b151.
    
    Revert "Add synchronized protection in BoostFramework constructor"
    
    This reverts commit a6c8c515162435a19375ed8bb0c4bd460dc948b2.
    
    Revert "Fix boost issue caused by pre-fling"
    
    This reverts commit bc961fc1e9d056844ecf80949d0d1b40128aa0b3.
    
    Revert "boost ahead of current fling boost in scroll"
    
    This reverts commit 08d0e822050eef1d8e64365c2b1a4bbc8761a53e.
    
    Revert "frameworks: Add custom boost for scroll/fling"
    
    This reverts commit 91338fb35fcc9a155345d284bc24fad86f4d33a8.
    
    Revert "Framework for LaunchBoost v2 and Frequency aggr during launch."
    
    This reverts commit c35293c3be2b5a0eaf1986b6772040a19f2d828a.
    
    Revert "Perf: Adding hooks for IO prefetcher into framework"
    
    This reverts commit 911884e18f8e7ec76bf9db5ea9a91e6f0d65ba6b.
    
    Revert "BoostFramework to enhance performance during critical scenarios"
    
    This reverts commit f56206d659dd686db3f7bf3c2660609917e9777d.

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -122,7 +122,7 @@ import android.util.Log;
 import android.util.Slog;
 import android.view.Display;
 import com.android.internal.app.ActivityTrigger;
-import android.util.BoostFramework;
+
 
 import com.android.internal.app.IVoiceInteractor;
 import com.android.internal.content.ReferrerIntent;
@@ -239,10 +239,6 @@ final class ActivityStack {
     final WindowManagerService mWindowManager;
     private final RecentTasks mRecentTasks;
 
-    public BoostFramework mPerf = null;
-    public boolean mIsAnimationBoostEnabled = false;
-    public int aBoostTimeOut = 0;
-    public int aBoostParamVal[];
     /**
      * The back history of all previous (and possibly still
      * running) activities.  It contains #TaskRecord objects.
@@ -457,14 +453,6 @@ final class ActivityStack {
         mRecentTasks = recentTasks;
         mTaskPositioner = mStackId == FREEFORM_WORKSPACE_STACK_ID
                 ? new LaunchingTaskPositioner() : null;
-        mIsAnimationBoostEnabled = mService.mContext.getResources().getBoolean(
-                   com.android.internal.R.bool.config_enablePerfBoostForAnimation);
-        if (mIsAnimationBoostEnabled) {
-           aBoostTimeOut = mService.mContext.getResources().getInteger(
-                   com.android.internal.R.integer.animationboost_timeout_param);
-           aBoostParamVal = mService.mContext.getResources().getIntArray(
-                   com.android.internal.R.array.animationboost_param_value);
-        }
     }
 
     void attachDisplay(ActivityStackSupervisor.ActivityDisplay activityDisplay, boolean onTop) {
@@ -2381,9 +2369,6 @@ final class ActivityStack {
         // that the previous one will be hidden soon.  This way it can know
         // to ignore it when computing the desired screen orientation.
         boolean anim = true;
-        if (mIsAnimationBoostEnabled == true && mPerf == null) {
-            mPerf = new BoostFramework();
-        }
         if (prev != null) {
             if (prev.finishing) {
                 if (DEBUG_TRANSITION) Slog.v(TAG_TRANSITION,
@@ -2395,9 +2380,6 @@ final class ActivityStack {
                     mWindowManager.prepareAppTransition(prev.task == next.task
                             ? TRANSIT_ACTIVITY_CLOSE
                             : TRANSIT_TASK_CLOSE, false);
-                    if(prev.task != next.task && mPerf != null) {
-                       mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
-                    }
                 }
                 mWindowManager.setAppVisibility(prev.appToken, false);
             } else {
@@ -2412,9 +2394,6 @@ final class ActivityStack {
                             : next.mLaunchTaskBehind
                                     ? TRANSIT_TASK_OPEN_BEHIND
                                     : TRANSIT_TASK_OPEN, false);
-                    if(prev.task != next.task && mPerf != null) {
-                        mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
-                    }
                 }
             }
         } else {
