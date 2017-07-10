30e83f780a3216cb4c879b055779e80d7c7effb1
joshuous <joshuous@gmail.com>
2017-07-10 15:24:11 +0000

    Display correct Power Off / Reboot message
    * Remove CAF leftovers
    * The "showRebootOption" was not working as it should. It'll always be
      true as long as a "reboot" option is possible, even when we want to
      just Power Off. More appropriate is to use mReboot to determine
      whether we really intend to reboot.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -159,25 +159,15 @@ public final class ShutdownThread extends Thread {
             }
         }
 
-        boolean showRebootOption = false;
-        String[] defaultActions = context.getResources().getStringArray(
-                com.android.internal.R.array.config_globalActionsList);
-        for (int i = 0; i < defaultActions.length; i++) {
-            if (defaultActions[i].equals("reboot")) {
-                showRebootOption = true;
-                break;
-            }
-        }
         final int longPressBehavior = context.getResources().getInteger(
                         com.android.internal.R.integer.config_longPressOnPowerBehavior);
         int resourceId = mRebootSafeMode
                 ? com.android.internal.R.string.reboot_safemode_confirm
-                : (longPressBehavior == 2
-                        ? com.android.internal.R.string.shutdown_confirm_question
-                        : com.android.internal.R.string.shutdown_confirm);
-        if (showRebootOption && !mRebootSafeMode) {
-            resourceId = com.android.internal.R.string.reboot_confirm;
-        }
+                : (mReboot
+                        ? com.android.internal.R.string.reboot_confirm
+                        : (longPressBehavior == 2
+                            ? com.android.internal.R.string.shutdown_confirm_question
+                            : com.android.internal.R.string.shutdown_confirm));
 
         final int titleResourceId = mRebootSafeMode
                  ? com.android.internal.R.string.reboot_safemode_title
