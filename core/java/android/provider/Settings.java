2f783863b89cc3c0ee9328286800e3097776ac3d
joshuous <joshuous@gmail.com>
2017-08-26 02:45:41 +0000

    Revert "Double tap to sleep on the statusbar [1/2]"
    This reverts commit c761521ed81d11c19cccb753d629c1827087f9fa.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3895,12 +3895,6 @@ public final class Settings {
         };
 
         /**
-         * Controls whether statusbar double tap to sleep is enabled
-         * @hide
-         */
-        public static final String GESTURE_DOUBLE_TAP_SLEEP = "gesture_double_tap_sleep";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
